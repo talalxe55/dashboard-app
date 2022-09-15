507f01891b9841e103ed23be90c0d0f5d4a31e2a
Hassan Khan <hassankhan427726@outlook.com>
2022-09-15 17:40:41 +0500

    Credit Card UI changed into Functional Component

--- src/theme/components/CreditCard.js
+++ src/theme/components/CreditCard.js
@@ -1,81 +1,92 @@
-import React from "react";
+import React, { useState, detailef, useCallback } from "react";
 import ReactCreditCards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import { FormControl, Flex, Input, Box, Stack } from "@chakra-ui/react";
 
-export class CreditCard extends React.Component {
-  state = {
+export const CreditCard = () => {
+  const [focus, setFocus] = useState("");
+  const [detail, setDetail] = useState({
     cvc: "",
     expiry: "",
     focus: "",
     name: "",
     number: "",
-  };
-  handleInputFocus = (e) => {
-    this.setState({ focus: e.target.name });
-  };
-  handleInputChange = (e) => {
+  });
+  const handleInputs = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-    console.log(name);
+    setDetail((preval) => {
+      return {
+        ...preval,
+        [name]: value,
+      };
+    });
+    console.log(e.target.value);
   };
-  render() {
-    return (
-      <Box>
-        <FormControl>
-          <Stack my={5}>
-            <ReactCreditCards
-              cvc={this.state.cvc}
-              expiry={this.state.expiry}
-              focused={this.state.focus}
-              name={this.state.name}
-              number={this.state.number}
-            />
-          </Stack>
-          <Flex gap={3}>
-            <Input
-              type="tel"
-              name="number"
-              className="form-control"
-              placeholder="Card Number"
-              pattern="[\d| ]{16,22}"
-              required
-              onChange={this.handleInputChange}
-              onFocus={this.handleInputFocus}
-            />
-            <Input
-              type="text"
-              name="name"
-              placeholder="Name"
-              required
-              onChange={this.handleInputChange}
-              onFocus={this.handleInputFocus}
-            />
-          </Flex>
-          <Flex gap={3} mt={3}>
-            <Input
-              type="tel"
-              name="expiry"
-              className="form-control"
-              placeholder="Valid Thru"
-              pattern="\d\d/\d\d"
-              required
-              onChange={this.handleInputChange}
-              onFocus={this.handleInputFocus}
-            />
-            <Input
-              type="tel"
-              name="cvc"
-              className="form-control"
-              placeholder="CVC"
-              pattern="\d{3,4}"
-              required
-              onChange={this.handleInputChange}
-              onFocus={this.handleInputFocus}
-            />
-          </Flex>
-        </FormControl>
-      </Box>
-    );
-  }
-}
+  const handleInputFocus = (e) => {
+    setFocus({ focus: e.target.name });
+    console.log(e.target.name);
+  };
+  return (
+    <Box>
+      <FormControl>
+        <Stack my={5}>
+          <ReactCreditCards
+            cvc={detail.cvc}
+            expiry={detail.expiry}
+            focused={focus.focus}
+            name={detail.name}
+            number={detail.number}
+          />
+        </Stack>
+        <Flex gap={3}>
+          <Input
+            type="tel"
+            name="number"
+            placeholder="Card Number"
+            pattern="[\d| ]{16,22}"
+            maxLength={16}
+            required
+            value={detail.number}
+            onChange={handleInputs}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="text"
+            name="name"
+            placeholder="Name"
+            maxLength={25}
+            required
+            value={detail.name}
+            onChange={handleInputs}
+            onFocus={handleInputFocus}
+          />
+        </Flex>
+        <Flex gap={3} mt={3}>
+          <Input
+            type="tel"
+            name="expiry"
+            placeholder="Valid Thru"
+            pattern="\d\d/\d\d"
+            maxLength={4}
+            minLength={4}
+            required
+            value={detail.expiry}
+            onChange={handleInputs}
+            onFocus={handleInputFocus}
+          />
+          <Input
+            type="tel"
+            name="cvc"
+            placeholder="CVC"
+            pattern="\d{3,4}"
+            maxLength={3}
+            required
+            value={detail.cvc}
+            onChange={handleInputs}
+            onFocus={handleInputFocus}
+          />
+        </Flex>
+      </FormControl>
+    </Box>
+  );
+};
--- src/theme/components/PaymentForm.js
+++ src/theme/components/PaymentForm.js
@@ -85,44 +85,45 @@ const PaymentForm = (props) => {
           <ModalCloseButton />
           <ModalBody pb={6} overflowY={{ sm: "auto" }}>
             <FormControl isRequired>
-              <FormLabel>Amount</FormLabel>
-              <Text mb={"-31px"} mt={4} ms={2} fontSize="small" color="gray">
-                {currency}
-              </Text>
-
-              <Flex gap={3}>
-                <Input
-                  ref={initialRef}
-                  type="number"
-                  name="amt"
-                  placeholder=""
-                  autoComplete="off"
-                  ps={12}
-                />
-                <Stack width={300}>
-                  <Select
-                    id="currencysym"
-                    icon={<TriangleDownIcon />}
-                    onChange={() =>
-                      setCurrency(
-                        document.getElementById("currencysym").value === "USD"
-                          ? "USD$"
-                          : document.getElementById("currencysym").value
-                      )
-                    }
-                  >
-                    {Currency.map((val, index) => {
-                      return (
-                        <option value={val.abbreviation} key={index}>
-                          {val.abbreviation + " - " + val.currency}
-                        </option>
-                      );
-                    })}
-                  </Select>
-                </Stack>
-              </Flex>
+              <Box>
+                <FormLabel>Amount</FormLabel>
+                <Text mb={"-31px"} mt={4} ms={2} fontSize="small" color="gray">
+                  {currency}
+                </Text>
+                <Flex gap={3}>
+                  <Input
+                    ref={initialRef}
+                    type="number"
+                    name="amt"
+                    placeholder=""
+                    autoComplete="off"
+                    ps={12}
+                  />
+                  <Stack width={300}>
+                    <Select
+                      id="currencysym"
+                      icon={<TriangleDownIcon />}
+                      onChange={() =>
+                        setCurrency(
+                          document.getElementById("currencysym").value === "USD"
+                            ? "USD$"
+                            : document.getElementById("currencysym").value
+                        )
+                      }
+                    >
+                      {Currency.map((val, index) => {
+                        return (
+                          <option value={val.abbreviation} key={index}>
+                            {val.abbreviation + " - " + val.currency}
+                          </option>
+                        );
+                      })}
+                    </Select>
+                  </Stack>
+                </Flex>
+              </Box>
             </FormControl>
-            <FormControl mt={4}>
+            <Box mt={4}>
               <Flex>
                 <FormLabel>Customer </FormLabel>
                 <Text color="gray">(Optional)</Text>
@@ -133,9 +134,9 @@ const PaymentForm = (props) => {
                 options={options}
                 placeholder="Find Customer"
               />
-            </FormControl>
+            </Box>
 
-            <FormControl mt={4}>
+            <Box mt={4}>
               <Flex alignItems={"center"}>
                 <FormLabel m={0} pe={4}>
                   Description
@@ -149,8 +150,8 @@ const PaymentForm = (props) => {
                 </Tooltip>
               </Flex>
               <Input placeholder="Products or services associated with payment" />
-            </FormControl>
-            <FormControl mt={4}>
+            </Box>
+            <Box mt={4}>
               <Flex alignItems={"center"}>
                 <FormLabel m={0} pe={4}>
                   Statement descriptor
@@ -164,43 +165,44 @@ const PaymentForm = (props) => {
                 </Tooltip>
               </Flex>
               <CustomControlsExample />
-            </FormControl>
-            <FormControl mt={4}>
-              <Heading fontSize="xl">Payment method</Heading>
-              <Divider my={4} />
-
-              <Radio value="master" defaultChecked="true">
-                Manually enter card information
-              </Radio>
-              {/* <Flex
-                p="1rem"
-                bg="transparent"
-                borderRadius="15px"
-                width="100%"
-                border="1px solid"
-                borderColor={"gray.400"}
-                align="center"
-                mb={{ sm: "24px", md: "0px" }}
-                me={{ sm: "0px", md: "24px" }}
-              >
-                <IconBox me="10px" w="25px" h="22px">
-                  <MastercardIcon w="100%" h="100%" />
-                </IconBox>
-                <Text color="gray.400" fontSize="md" fontWeight="semibold">
-                  7812 2139 0823 XXXX
-                </Text>
-                <Spacer />
-                <Button
-                  p="0px"
+            </Box>
+            <Box>
+              <FormControl mt={4}>
+                <Heading fontSize="xl">Payment method</Heading>
+                <Divider my={4} />
+                <Radio value="master" defaultChecked="true">
+                  Manually enter card information
+                </Radio>
+                {/* <Flex
+                  p="1rem"
                   bg="transparent"
-                  w="16px"
-                  h="16px"
-                  variant="no-hover"
+                  borderRadius="15px"
+                  width="100%"
+                  border="1px solid"
+                  borderColor={"gray.400"}
+                  align="center"
+                  mb={{ sm: "24px", md: "0px" }}
+                  me={{ sm: "0px", md: "24px" }}
                 >
-                  <Icon as={FaPencilAlt} />
-                </Button>
-              </Flex> */}
-            </FormControl>
+                  <IconBox me="10px" w="25px" h="22px">
+                    <MastercardIcon w="100%" h="100%" />
+                  </IconBox>
+                  <Text color="gray.400" fontSize="md" fontWeight="semibold">
+                    7812 2139 0823 XXXX
+                  </Text>
+                  <Spacer />
+                  <Button
+                    p="0px"
+                    bg="transparent"
+                    w="16px"
+                    h="16px"
+                    variant="no-hover"
+                  >
+                    <Icon as={FaPencilAlt} />
+                  </Button>
+                </Flex> */}
+              </FormControl>
+            </Box>
             <Box>
               <CreditCard />
             </Box>
