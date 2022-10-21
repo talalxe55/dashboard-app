b5d37c3d61e9d96b0b8e41429faff514c2eee982
Hassan Khan <hassankhan427726@outlook.com>
2022-10-21 18:54:04 +0500

    Card issue resolved from Table.js - Customer listing

--- src/views/Dashboard/Billing.js
+++ src/views/Dashboard/Billing.js
@@ -117,7 +117,6 @@ function Billing() {
 
   const CardDetails = () => {
     if (singleCustomerSources) {
-      // console.log(Object.keys(singleCustomerSources));
       if (singleCustomerSources.data.length > 0) {
         if (Object.keys(singleCustomerSources.data[0].card) !== 0) {
           if (singleCustomerSources.data[0].card.last4.length > 0) {
@@ -360,68 +359,72 @@ function Billing() {
                 <Text fontSize="lg" color={textColor} fontWeight="bold">
                   Payment Method
                 </Text>
+                {/* Create Payment Modal */}
                 <PaymentForm bg={bgButton} />
               </Flex>
             </CardHeader>
-            {singleCustomerSources ? (
-              singleCustomerSources.data.length > 0 ? (
-                singleCustomerSources.data.map((val, index) => {
-                  if (val.card !== undefined) {
-                    return (
-                      <CardBody key={index}>
-                        <Flex
-                          direction={{ sm: "column", md: "row" }}
-                          align="center"
-                          w="100%"
-                          justify="center"
-                          py="1rem"
-                        >
+            <Grid gap={5} templateColumns={{ sm: "1fr", lg: "repeat(2, 1fr)" }}>
+              {/* {console.log(singleCustomerSources.data)} */}
+              {singleCustomerSources ? (
+                singleCustomerSources.data.length > 0 ? (
+                  singleCustomerSources.data.map((val, index) => {
+                    if (val.card !== undefined) {
+                      return (
+                        <CardBody key={index}>
                           <Flex
-                            p="1rem"
-                            bg="transparent"
-                            borderRadius="15px"
-                            width="100%"
-                            border="1px solid"
-                            borderColor={borderColor}
+                            direction={{ sm: "column", md: "row" }}
                             align="center"
-                            mb={{ sm: "24px", md: "0px" }}
-                            me={{ sm: "0px", md: "24px" }}
+                            w="100%"
+                            justify="center"
+                            py="1rem"
                           >
-                            <IconBox me="10px" w="25px" h="22px">
-                              {val ? (
-                                (val.card.brand === "Visa",
-                                (<VisaIcon w="100%" h="100%" />))
-                              ) : (
-                                <VisaIcon w="100%" h="100%" />
-                              )}
-                            </IconBox>
-                            <Text
-                              color="gray.400"
-                              fontSize="md"
-                              fontWeight="semibold"
-                            >
-                              {"XXXX XXXX XXXX " + val.card.last4}
-                            </Text>
-                            <Spacer />
-                            <Button
-                              p="0px"
+                            <Flex
+                              p="1rem"
                               bg="transparent"
-                              w="16px"
-                              h="16px"
-                              variant="no-hover"
-                            ></Button>
+                              borderRadius="15px"
+                              width="100%"
+                              border="1px solid"
+                              borderColor={borderColor}
+                              align="center"
+                              mb={{ sm: "24px", md: "0px" }}
+                              me={{ sm: "0px", md: "24px" }}
+                            >
+                              <IconBox me="10px" w="25px" h="22px">
+                                {val ? (
+                                  (val.card.brand === "Visa",
+                                  (<VisaIcon w="100%" h="100%" />))
+                                ) : (
+                                  <VisaIcon w="100%" h="100%" />
+                                )}
+                              </IconBox>
+                              <Text
+                                color="gray.400"
+                                fontSize="md"
+                                fontWeight="semibold"
+                              >
+                                {"XXXX XXXX X12XXX " + val.card.last4}
+                              </Text>
+                              <Spacer />
+                              <Button
+                                p="0px"
+                                bg="transparent"
+                                w="16px"
+                                h="16px"
+                                variant="no-hover"
+                              ></Button>
+                            </Flex>
                           </Flex>
-                        </Flex>
-                      </CardBody>
-                    );
-                  }
-                })
+                        </CardBody>
+                      );
+                    }
+                  })
+                ) : (
+                  "No Card found!"
+                )
               ) : (
-                "No Card found!"
-              )
-            ) : (
-              <SkeletonText noOfLines={3} />
-            )}
+                <SkeletonText noOfLines={3} />
+              )}
+            </Grid>
           </Card>
         </Box>
         <Card my="24px" ms={{ lg: "24px" }}>
@@ -499,18 +502,14 @@ function Billing() {
           </CardBody>
         </Card>
       </Grid>
-      {/* BILLING SOURCES GRID STARTED */}
-
+      {/* BILLING SOURCES*/}
       <Card my={{ lg: "24px" }} me={{ lg: "24px" }}>
         <CardHeader py="12px">
           <Text color={textColor} fontSize="lg" fontWeight="bold">
             Billing Sources
           </Text>
         </CardHeader>
-        <Grid
-          gap={5}
-          templateColumns={{ sm: "1fr", lg: "repeat(2, 1fr)" }}
-        >
+        <Grid gap={5} templateColumns={{ sm: "1fr", lg: "repeat(2, 1fr)" }}>
           {singleCustomerSources ? (
             singleCustomerSources.data.length > 0 ? (
               singleCustomerSources.data.map((val, index) => {
@@ -520,7 +519,7 @@ function Billing() {
                   }
                   return (
                     <>
-                      <CardBody id="cardbody">
+                      <CardBody>
                         <Flex direction="column" w="100%">
                           <BillingRowSources
                             key={index}
